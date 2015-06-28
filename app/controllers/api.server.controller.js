88863b79b8e09df642f9fd7a471ad5c9b746327c
ryhamz <ryhamz@gmail.com>
2015-06-28 07:00:53 +0000

    Added uber req example

--- app/controllers/api.server.controller.js
+++ app/controllers/api.server.controller.js
@@ -15,3 +15,28 @@ exports.accept = function(req, res) {
     res.set('Content-Type', 'text/xml');
     res.sendFile(__dirname + '/1.xml');
 };
+
+exports.uberAuth = function(req, res) {
+  var request = require("request")
+
+request({
+  url: 'https://api.uber.com/v1/products',
+  method: "GET",
+  qs: {
+  'server_token': 'IjJKxQTB9RT-I6rSlvUalipTWlKdTSaf5GF-Cv29',
+  'latitude': 41.9373,
+  'longitude': -87.6551
+  }
+
+  }, function(err, resp, body){
+   if (err)
+    console.error(err)
+  console.log(body)
+}
+        
+       )
+};
+
+exports.uberOAuth2 = function (req, resp){
+  
+};
---
+++ app/controllers/scrap.js
@@ -0,0 +1,19 @@
+var request = require("request")
+
+request({
+  url: 'https://api.uber.com/v1/products',
+  method: "GET",
+  qs: {
+  'server_token': 'IjJKxQTB9RT-I6rSlvUalipTWlKdTSaf5GF-Cv29',
+  'latitude': 41.9373,
+  'longitude': -87.6551
+  }
+
+  
+}, function(err, req, body){
+  if (err)
+    console.error(err)
+  console.log(body)
+}
+        
+       )
