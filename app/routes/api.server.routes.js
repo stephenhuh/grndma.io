aea26d7ab8b7bae510cad678df019c2846635a0f
Stephen Huh <stephenhuh@gmail.com>
2015-06-28 00:27:50 -0500

    integrate basic calling & greeting

--- app/routes/api.server.routes.js
+++ app/routes/api.server.routes.js
@@ -4,5 +4,7 @@ module.exports = function(app) {
     var api = require('../../app/controllers/api.server.controller');
     app.route('/api')
         .get(api.index);
-
+    app.route('/api/accept') 
+        .get(api.getaccept)
+        .post(api.accept);
 };
