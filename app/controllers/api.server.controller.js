3b3bc1168c676eda4a31de3cbf56ff697b2caeb0
Stephen Huh <stephenhuh@gmail.com>
2015-06-28 10:14:26 -0500

    build basic phone functionality

--- app/controllers/api.server.controller.js
+++ app/controllers/api.server.controller.js
@@ -15,3 +15,15 @@ exports.accept = function(req, res) {
     res.set('Content-Type', 'text/xml');
     res.sendFile(__dirname + '/1.xml');
 };
+
+exports.handle = function(req, res){
+    if (req.body.Digits == 1){
+        res.set('Content-Type', 'test/xml');
+        res.send('<Response><Say>YOOOOOO U CALLED AN UBER U CALLED AN UBER </Say></Response>');
+    }
+    else{
+        res.set('Content-Type', 'test/xml');
+        res.send('<Response><Say>U MISSED U MISSED U MISSED U MISSED</Say></Response>')
+    }
+};
+
--- app/routes/api.server.routes.js
+++ app/routes/api.server.routes.js
@@ -1,10 +1,17 @@
 'use strict';
-
 module.exports = function(app) {
     var api = require('../../app/controllers/api.server.controller');
+    var bodyParser = require('body-parser');
+    var multer = require('multer'); 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(multer());
+
     app.route('/api')
         .get(api.index);
-    app.route('/api/accept') 
+    app.route('/api/accept')
         .get(api.getaccept)
         .post(api.accept);
+    app.route('/api/accept/handle')
+        .post(api.handle);
 };
