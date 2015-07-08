52d89732faab85cde5253839af9ace53df84288e
kmattimo <kmattimo@umich.edu>
2015-07-07 22:07:09 -0500

    updated grandma UI to match model

--- app/models/grandma.server.model.js
+++ app/models/grandma.server.model.js
@@ -54,12 +54,16 @@ var GrandmaSchema = new Schema({
 		},
 	},
 
-	rootTreeMenu: { type: Schema.Types.ObjectId, ref: 'treeMenu' }
-	//rootTreeMenu: [mongoose.model('treeMenu').schema]
+//	rootTreeMenu: { type: Schema.Types.ObjectId, ref: 'treeMenu' }
+//apparently you can't have single (non-aray) external schema definitions.
+//but including it is the nosql way as far as I can tell.
+//so will address as rootTreeMenu[0]
+rootTreeMenu: [mongoose.model('treeMenu').schema]
 });
 
 GrandmaSchema.virtual('address.full').get(function() {
-	return this.address.street + ',  ' + this.address.city + ', ' + this.address.state + ' ' + this.address.zip ;
+	return this.address.street + '\n  ' + this.address.city + ', ' + this.address.state + ' ' + this.address.zip ;
 });
+GrandmaSchema.set('toJSON', { virtuals: true });
 
 mongoose.model('Grandma', GrandmaSchema);
