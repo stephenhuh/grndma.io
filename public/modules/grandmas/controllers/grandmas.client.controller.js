1fd894b34bc2153d7a95c9d6fb0c9af1608199e3
kmattimo <kmattimo@umich.edu>
2015-07-13 22:57:21 -0500

    using angular-ui-tree

--- public/modules/grandmas/controllers/grandmas.client.controller.js
+++ public/modules/grandmas/controllers/grandmas.client.controller.js
@@ -6,6 +6,50 @@ angular.module('grandmas').controller('GrandmasController', ['$scope', '$statePa
 		$scope.authentication = Authentication;
 
 		$scope.showGrandmaMetadata = false;
+		$scope.list = [
+  {
+    "id": 1,
+    "title": "1. dragon-breath",
+    "items": []
+  },
+  {
+    "id": 2,
+    "title": "2. moiré-vision",
+    "items": [
+      {
+        "id": 21,
+        "title": "2.1. tofu-animation",
+        "items": [
+          {
+            "id": 211,
+            "title": "2.1.1. spooky-giraffe",
+            "items": []
+          },
+          {
+            "id": 212,
+            "title": "2.1.2. bubble-burst",
+            "items": []
+          }
+        ]
+      },
+      {
+        "id": 22,
+        "title": "2.2. barehand-atomsplitting",
+        "items": []
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "title": "3. unicorn-zapper",
+    "items": []
+  },
+  {
+    "id": 4,
+    "title": "4. romantic-transclusion",
+    "items": []
+  }
+];
 
 		// Create new Grandma
 		$scope.create = function() {
