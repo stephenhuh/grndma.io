c44a1f069b330fc6fed4756f2a6f95a67fb14b31
kmattimo <kmattimo@umich.edu>
2015-07-14 22:44:44 -0500

    NOW FEATURING ANGULAR-UI-TREE

--- public/modules/grandmas/controllers/grandmas.client.controller.js
+++ public/modules/grandmas/controllers/grandmas.client.controller.js
@@ -8,46 +8,46 @@ angular.module('grandmas').controller('GrandmasController', ['$scope', '$statePa
 		$scope.showGrandmaMetadata = false;
 		$scope.list = [
   {
-    "id": 1,
-    "title": "1. dragon-breath",
-    "items": []
+    'id': 1,
+    'title': '1. dragon-breath',
+    'items': []
   },
   {
-    "id": 2,
-    "title": "2. moiré-vision",
-    "items": [
+    'id': 2,
+    'title': '2. moiré-vision',
+    'items': [
       {
-        "id": 21,
-        "title": "2.1. tofu-animation",
-        "items": [
+        'id': 21,
+        'title': '2.1. tofu-animation',
+        'items': [
           {
-            "id": 211,
-            "title": "2.1.1. spooky-giraffe",
-            "items": []
+            'id': 211,
+            'title': '2.1.1. spooky-giraffe',
+            'items': []
           },
           {
-            "id": 212,
-            "title": "2.1.2. bubble-burst",
-            "items": []
+            'id': 212,
+            'title': '2.1.2. bubble-burst',
+            'items': []
           }
         ]
       },
       {
-        "id": 22,
-        "title": "2.2. barehand-atomsplitting",
-        "items": []
+        'id': 22,
+        'title': '2.2. barehand-atomsplitting',
+        'items': []
       }
     ]
   },
   {
-    "id": 3,
-    "title": "3. unicorn-zapper",
-    "items": []
+    'id': 3,
+    'title': '3. unicorn-zapper',
+    'items': []
   },
   {
-    "id": 4,
-    "title": "4. romantic-transclusion",
-    "items": []
+    'id': 4,
+    'title': '4. romantic-transclusion',
+    'items': []
   }
 ];
 
