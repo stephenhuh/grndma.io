fa571ed582b211b6fce60a2eacf95d7e7f7ae036
Aaron Leon <aaronleon2018@u.northwestern.edu>
2015-08-10 20:19:37 -0500

    Rehashed styling

--- config/env/all.js
+++ config/env/all.js
@@ -45,6 +45,8 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/bootstrap-social/bootstrap-social.css',
+				'public/lib/font-awesome/css/font-awesome.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -53,7 +55,7 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				
+
 				'public/lib/angular-ui-tree/dist/angular-ui-tree.js'
 			]
 		},
